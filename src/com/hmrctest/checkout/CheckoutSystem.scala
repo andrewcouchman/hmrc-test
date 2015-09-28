4e9cd79633dc91928da8929dfb974fd7329036db
AndrewC <andrew.couchman@gmail.com>
2015-09-28 21:49:04 +0100

    test failing with new bogof rule created assertEquals method to give better test feedback

--- src/com/hmrctest/checkout/CheckoutSystem.scala
+++ src/com/hmrctest/checkout/CheckoutSystem.scala
@@ -1,9 +1,7 @@
 package com.hmrctest.checkout
 
-import scala.collection.immutable.IndexedSeq
-
 class CheckoutSystem() {
-  def processProducts(productList: IndexedSeq[String]):BigDecimal = {
+  def processProducts(productList: Seq[String]):BigDecimal = {
     productList.map {
       case "Apple" => BigDecimal("0.6")
       case _ => BigDecimal("0.25")
--- test/com/hmrctest/checkout/CheckoutEndToEndTest.scala
+++ test/com/hmrctest/checkout/CheckoutEndToEndTest.scala
@@ -4,32 +4,47 @@ object CheckoutEndToEndTest extends App {
   runTest()
 
   def runTest(): Unit = {
-    tenApplesShouldCostSixPounds()
-    fiveApplesShouldCostThreePounds()
+    oneAppleShouldCostSixtyPence()
+    twoApplesShouldCostSixtyPence()
+    tenApplesShouldCostThreePounds()
+
     fiveOrangesShouldCostOnePoundTwentyFive()
     aMixOfThreeOrangesAndFourApplesCostsThreePoundsFifteen()
   }
-  def tenApplesShouldCostSixPounds(){
-    val productList = (1 to 10).map(_ => "Apple")
+
+  def oneAppleShouldCostSixtyPence(): Unit = {
+    val productList = List("Apple")
     val totalPrice = new CheckoutSystem().processProducts(productList)
-    assert(totalPrice == BigDecimal(6))
+    assertEquals(totalPrice, BigDecimal(0.6))
   }
 
-  def fiveApplesShouldCostThreePounds(): Unit = {
-    val productList = (1 to 5).map(_ => "Apple")
+  def twoApplesShouldCostSixtyPence(): Unit = {
+    val productList = (1 to 2).map(_ => "Apple")
     val totalPrice = new CheckoutSystem().processProducts(productList)
-    assert(totalPrice == BigDecimal(3))
+    assertEquals(totalPrice, BigDecimal(0.6))
+  }
+
+  def tenApplesShouldCostThreePounds() {
+    val productList = (1 to 10).map(_ => "Apple")
+    val totalPrice = new CheckoutSystem().processProducts(productList)
+    assertEquals(totalPrice, BigDecimal(3))
   }
 
   def fiveOrangesShouldCostOnePoundTwentyFive(): Unit = {
     val productList = (1 to 5).map(_ => "Orange")
     val totalPrice = new CheckoutSystem().processProducts(productList)
-    assert(totalPrice == BigDecimal(1.25))
+    assertEquals(totalPrice, BigDecimal(1.25))
   }
 
   def aMixOfThreeOrangesAndFourApplesCostsThreePoundsFifteen(): Unit = {
     val productList = (1 to 3).map(_ => "Orange") ++ (1 to 4).map(_ => "Apple")
     val totalPrice = new CheckoutSystem().processProducts(productList)
-    assert(totalPrice == BigDecimal(3.15))
+    assertEquals(totalPrice, BigDecimal(3.15))
+  }
+
+  def assertEquals(first: Any, second: Any) = {
+    if (first != second) {
+      throw new AssertionError(first + " != " + second)
+    }
   }
 }
