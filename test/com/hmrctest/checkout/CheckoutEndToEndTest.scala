71120d2258289500bf8df2903976ccfe5fab154d
AndrewC <andrew.couchman@gmail.com>
2015-09-28 21:01:43 +0100

    second failing test

--- test/com/hmrctest/checkout/CheckoutEndToEndTest.scala
+++ test/com/hmrctest/checkout/CheckoutEndToEndTest.scala
@@ -5,10 +5,18 @@ object CheckoutEndToEndTest extends App {
 
   def runTest(): Unit = {
     tenApplesShouldCostSixPounds()
+    fiveApplesShouldCostThreePounds()
   }
   def tenApplesShouldCostSixPounds(){
     val productList = (1 to 10).map(_ => "Apple")
     val totalPrice = new CheckoutSystem().processProducts(productList)
     assert(totalPrice == BigDecimal(6))
+  }
+
+  def fiveApplesShouldCostThreePounds(): Unit = {
+    val productList = (1 to 5).map(_ => "Apple")
+    val totalPrice = new CheckoutSystem().processProducts(productList)
+    assert(totalPrice == BigDecimal(3))
+
   }
 }
