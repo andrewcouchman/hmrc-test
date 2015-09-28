9d3e18695e45eb9c8504a6bbf93c515043da8827
AndrewC <andrew.couchman@gmail.com>
2015-09-28 21:33:19 +0100

    now copes with mixed basket

--- src/com/hmrctest/checkout/CheckoutSystem.scala
+++ src/com/hmrctest/checkout/CheckoutSystem.scala
@@ -4,9 +4,9 @@ import scala.collection.immutable.IndexedSeq
 
 class CheckoutSystem() {
   def processProducts(productList: IndexedSeq[String]):BigDecimal = {
-    productList.head match{
-      case "Apple" => BigDecimal("0.6") * productList.size
-      case _ => BigDecimal("0.25") * productList.size
-    }
+    productList.map {
+      case "Apple" => BigDecimal("0.6")
+      case _ => BigDecimal("0.25")
+    }.sum
   }
 }
