6d3c0d9bb6416df7ea03d10674be44f5f22a7347
AndrewC <andrew.couchman@gmail.com>
2015-09-28 21:22:02 +0100

    fixed test - can cope with Apples or Oranges

--- src/com/hmrctest/checkout/CheckoutSystem.scala
+++ src/com/hmrctest/checkout/CheckoutSystem.scala
@@ -3,6 +3,11 @@ package com.hmrctest.checkout
 import scala.collection.immutable.IndexedSeq
 
 class CheckoutSystem() {
-  def processProducts(productList: IndexedSeq[String]):BigDecimal = BigDecimal("0.6") * productList.size
+  def processProducts(productList: IndexedSeq[String]):BigDecimal = {
+    productList.head match{
+      case "Apple" => BigDecimal("0.6") * productList.size
+      case _ => BigDecimal("0.25") * productList.size
+    }
+  }
 
 }
