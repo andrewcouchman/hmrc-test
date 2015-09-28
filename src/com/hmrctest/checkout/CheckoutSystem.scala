eb5b4648416d65478d8e6ba10352aa7103957e16
AndrewC <andrew.couchman@gmail.com>
2015-09-28 21:04:54 +0100

    second failing test fixed - single product type now calculated correctly

--- src/com/hmrctest/checkout/CheckoutSystem.scala
+++ src/com/hmrctest/checkout/CheckoutSystem.scala
@@ -3,6 +3,6 @@ package com.hmrctest.checkout
 import scala.collection.immutable.IndexedSeq
 
 class CheckoutSystem() {
-  def processProducts(productList: IndexedSeq[String]):BigDecimal = BigDecimal(6)
+  def processProducts(productList: IndexedSeq[String]):BigDecimal = BigDecimal("0.6") * productList.size
 
 }
